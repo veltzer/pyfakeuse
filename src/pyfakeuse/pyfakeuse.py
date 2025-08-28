ae6cfecea0fbe689f4515477e75b30c4145b674e
Mark Veltzer <mark.veltzer@gmail.com>
2025-08-28 23:41:44 +0300

    no commit message given

--- src/pyfakeuse/pyfakeuse.py
+++ src/pyfakeuse/pyfakeuse.py
@@ -1,7 +1,7 @@
 """ pyfakeuse.py """
 
 # The next line is needed so that pycharm will not complain about
-# the fact that we do not use the 'p' parameter.
+# the fact that we do not use the "p" parameter.
 
 
 # noinspection PyUnusedLocal
