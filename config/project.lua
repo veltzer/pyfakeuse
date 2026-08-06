fdd79249183103ae5f6518571676522864336a89
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:22 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,11 @@
+NAME = "pyfakeuse"
+DESCRIPTION_SHORT = "pyfakeuse helps you fool development environments and code inspectors"
+KEYWORDS = {
+    "python",
+    "fake",
+    "use",
+    "variables",
+    "idea",
+    "pycharm",
+    "fool",
+}
